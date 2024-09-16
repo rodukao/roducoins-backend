ef353f47ef7ba9d9ca08927c7ef9b1e058f92520
rodukao <rodukao@gmail.com>
2024-09-16 10:51:18 -0300

    Inicialização do projeto backend

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Servidor Roducoins está rodando!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+//conexão com banco
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Conectado no MongoDB'))
+.catch((err) => console.log(err));
